cb56bcca20a09f88d87635f28f0153b736490c25
psomers3 <peter.somers@isys.uni-stuttgart.de>
2022-07-06 08:20:29 +0200

    made example more interesting

--- examples/video_example.py
+++ examples/video_example.py
@@ -1,11 +1,21 @@
 from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
 from media_viewing_widgets import VideoPlayer
 
 
 if __name__ == '__main__':
     app = QApplication(["test"])
     vid = VideoPlayer()
-    vid.frame_grabbed.connect(lambda x, t: print(f'got frame at time {t}'))
+    img = QLabel()
+
+    def popup(image: QImage, t):
+        image = image.scaled(800, 600, Qt.AspectRatioMode.KeepAspectRatio)
+        pix = QPixmap.fromImage(image)
+        img.setPixmap(pix)
+        img.show()
+
+    vid.frame_grabbed.connect(popup)
     vid.set_video(QFileDialog().getOpenFileName()[0])
     vid.show()
     vid.play()
