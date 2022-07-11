8bf90b877b08b51721e9cc24de9e42b77c91e754
psomers3 <peter.somers@isys.uni-stuttgart.de>
2022-07-11 15:23:16 +0200

    added image view widget

--- examples/image_example.py
+++ examples/image_example.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import *
-from media_viewing_widgets import SlideView
+from media_viewing_widgets import ImageView
 from graphics_view import GraphicsView
 
 
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # may need High DPI scaling
     app = QApplication(['test'])
 
-    slide_view = SlideView()
+    slide_view = ImageView()
 
     scene = QGraphicsScene()
     scene.addItem(slide_view)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     viewer = GraphicsView(scene)
     viewer.resize(1000, 600)
     viewer.show()
-    slide_view.load_new_image(QFileDialog().getOpenFileName()[0])
+    slide_view.set_image(QFileDialog().getOpenFileName()[0])
     viewer.fitInView()
 
     app.exec()
--- media_viewing_widgets/__init__.py
+++ media_viewing_widgets/__init__.py
@@ -1,4 +1,5 @@
 from .slide_view import SlideView
 from .slideloader import SlideLoader
 from .video_display import VideoPlayer
+from .image_view import ImageView
 
